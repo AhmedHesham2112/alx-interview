3688c013c3f5707895fd22d4020ae77bd8be446b
AhmedHesham2112 <AhmedHesham2112>
2024-07-23 16:40:29 +0300

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 Pascal's Triangle
 """
 
+
 def pascal_triangle(n):
     """
     returns a list of lists of integers representing
@@ -14,7 +15,8 @@ def pascal_triangle(n):
     for row in range(len(triangle), n):
         current_row = [1]
         for element in range(1, row):
-            current_row.append(triangle[row - 1][element] + triangle[row - 1][element - 1])
+            current_row.append
+            (triangle[row - 1][element] + triangle[row - 1][element - 1])
         current_row.append(1)
         triangle.append(current_row)
     return triangle
