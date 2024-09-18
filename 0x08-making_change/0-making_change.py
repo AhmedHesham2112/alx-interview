68c1be7415357b1ff14bb2630d76ff2199456f14
AhmedHesham2112 <AhmedHesham2112>
2024-09-19 00:36:52 +0300

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -16,6 +16,8 @@ def makeChange(coins, total):
 
     for coin in coins:
         for i in range(coin, total + 1):
+            if dp[i] == dp[i - coin] + 1:
+                continue
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
